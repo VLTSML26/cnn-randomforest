c1d9fa00abac2dc3f1cc52abdb08e3eadde1ce5a
VLTSML26 <samu.voltan@gmail.com>
2022-10-03 15:55:31 +0200

    Virtual class first commit

--- cnn.py
+++ cnn.py
@@ -1,4 +1,7 @@
+from abc import ABC
+from random import Random
 import tensorflow as tf
+import pandas as pd
 from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,33 +13,58 @@ from sklearn import ensemble
 
 from data_loader import DataLoader
 
-class CNN(object):
+class Classifier:
+
+    def __init__(self, dataset):
+        self.data = DataLoader(dataset)
+        self.model = self.create_model()
+
+    def create_model(self):
+        pass
+
+    def train(self):
+        pass
+
+    def evaluate(self):
+        try:
+            return self.model.evaluate(self.data.x_test, self.data.y_test)
+        except:
+            return self.model.score(
+                self.data.x_test.reshape(len(self.data.x_test), self.data.img_cols * self.data.img_rows),
+                self.data.y_test
+            )
+
+    def summary(self):
+        try:
+            return self.model.summary()
+        except:
+            pass
+
+    # def table_evaluation(self):
+    #     evaluate = self.evaluate()
+    #     summary = {
+    #         'Loss': evaluate[0],
+    #         'Accuracy': evaluate[1],
+    #     }
+    #     df = pd.DataFrame.from_dict(summary)
+    #     df = df.append({
+    #         'Loss': tmp[0],
+    #         'Accuracy': tmp[1],
+    #     })
+
+    #     yield df
+
+class CNN(Classifier):
     epochs = None
     dropout = None
     batch_size = None
     optimizer = None
-    dataset = None
 
-    def __init__(self, config_dict):
-        """
-        Constructor.
-        
-        Parameters
-        ----------
-        config_dict: dictionary for configuration of the neural network.
-        """
-        tf.random.set_seed(0)
+    def __init__(self, dataset, config_dict):
         self.__dict__.update(config_dict)
-        self.data = DataLoader(self.dataset)
-        self.model = self.create_model()
-
-    def get_optimizer(self):
-        return self.optimizer
-
+        super().__init__(dataset)
+        
     def create_model(self):
-        """
-        Create CNN with the following specs:
-        """
         model = Sequential()
         model.add(
             Conv2D(
@@ -67,10 +95,7 @@ class CNN(object):
         )
         return model
 
-    def summary(self):
-        return self.model.summary()
-
-    def fit(self, verbose: int = 0):
+    def train(self, verbose: int = 0):
         return self.model.fit(
             self.data.x_train,
             self.data.y_train,
@@ -80,22 +105,20 @@ class CNN(object):
             validation_data=(self.data.x_test, self.data.y_test)
         )
 
-    def evaluate(self):
-        return self.model.evaluate(self.data.x_test, self.data.y_test)
-        
-class RandomForest(object):
+class RandomForest(Classifier):
     n_estimators = None
-    dataset = None
 
-    def __init__(self, config_dict):
+    def __init__(self, dataset, config_dict):
         self.__dict__.update(config_dict)
-        self.data = DataLoader(self.dataset)
-        self.model = ensemble.RandomForestClassifier(self.n_estimators)
+        super().__init__(dataset)
+        
+    def create_model(self):
+        return ensemble.RandomForestClassifier(self.n_estimators)
     
     # TODO: the constructor should reshape data, not the method. Also might be
     # better if DataLoader could pass already well-shaped data by accessing info
     # about the class calling it
-    def fit(self):
+    def train(self):
         self.model.fit(
             self.data.x_train.reshape(len(self.data.x_train), self.data.img_cols * self.data.img_rows),
             self.data.y_train
@@ -103,12 +126,20 @@ class RandomForest(object):
 
 def main():
     df = {
-        'n_estimators': 100,
-        'dataset': keras.datasets.fashion_mnist
+        'epochs': 5,
+        'dropout': 0.2,
+        'batch_size': 32,
+        'optimizer': 'SGD',
     }
-    aa = RandomForest(df)
-    bb = aa.fit()
-    import ipdb; ipdb.set_trace()
+    aa = CNN(keras.datasets.fashion_mnist, df)
+    aa.train(verbose=1)
+    print(aa.evaluate())
+    df2 = {
+        'n_estimators': 100
+    }
+    bb = RandomForest(keras.datasets.fashion_mnist, df2)
+    bb.train()
+    print(bb.evaluate())
 
 if __name__ == '__main__':
     main()
